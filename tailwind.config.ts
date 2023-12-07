17219b7b5e66737c90c9780b1a8583950ce890d8
nakamoto <zksytmkn4869@gmail.com>
2023-12-08 01:14:28 +0900

    💄

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const { nextui } = require("@nextui-org/react");
+
+export default {
+  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+} satisfies Config;
